84c258d326e20706bf6fc703946b478b40c18bc6
alangkl <alangramlichklein@gmail.com>
2023-04-08 17:55:01 -0300

    Se inicia JuegoGuerra

--- TP1PROBLEMA2/modules/JuegoGuerra.py
+++ TP1PROBLEMA2/modules/JuegoGuerra.py
@@ -8,6 +8,23 @@ Created on Thu Mar 30 18:49:28 2023
 import sys
 sys.path.append("C:/REPOSITORIOS/Algoritmos-y-estructuras-de-datos/TP1PROBLEMA1")
 from ListaDobleEnlazada import *
+import random
+
+
+valores = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'] 
+palos = ['♠', '♥', '♦', '♣']
+
+
+
 
 class JuegoGuerra:
+    def __init__(self, p_semilla = 0):
+    	self.semilla = p_semilla
+
+
+    def inicializar_mazo():
+    	cartas_en_orden = [(valor, palo) for valor in valores for palo in palos]
+    	random.seed(self.semilla)
+    	# aquí se inicializa el mazo de cartas
+    	random.shuffle(cartas)
     
